098c6179b432c7b0666bcb3e0e1910eb281c08df
unknown <thuanminh304@gmail.com>
2021-09-13 21:45:56 +0700

    component-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { Exercise1Component } from './exercise1/exercise1.component';
+import { HeaderComponent } from './exercise1/header/header.component';
+import { ContentComponent } from './exercise1/content/content.component';
+import { SidebarComponent } from './exercise1/sidebar/sidebar.component';
+import { FooterComponent } from './exercise1/footer/footer.component';
+//@NgModule: decorator
+//các option trong decorator được gọi là metadata
 @NgModule({
   declarations: [
-    AppComponent
+    // nơi khai báo tất cả component đc quản lý bởi module này
+    AppComponent,
+    Exercise1Component,
+    HeaderComponent,
+    ContentComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
+    // nơi khai báo những module đc sử dụng trong module này
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    // nơi khai báo service  
+  ],
+  bootstrap: [
+    //nơi khai báo component chạy đầu tiên trong module này
+    AppComponent]
 })
 export class AppModule { }
